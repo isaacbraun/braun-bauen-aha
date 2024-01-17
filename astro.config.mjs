e69ce24cf46b7a015a70357c1286bf3bfacfab5e
Isaac Braun <isaacbraun2000@gmail.com>
2024-01-16 20:31:17 -0500

    changed to prerendered

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import alpine from '@astrojs/alpinejs';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [alpine()],
+    output: 'hybrid',
+    adapter: netlify(),
 });
